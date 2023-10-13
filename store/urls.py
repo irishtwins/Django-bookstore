af004239a18680c40bd49b47dde632ec2a73b7f2
Christian Flores <christianflores@Christians-MacBook-Air.local>
2023-10-13 09:31:40 -0700

    modify path

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('store/', views.store, name='store'),
     path('store/new', views.new_book, name='new_book'),
     path('store/<int:book_id>', views.book_details, name='book_details'),
+    path('store/<int:book_id>/edit', views.edit_book, name='edit_book'),
+    path('store/<int:book_id>/delete', views.delete_book, name='delete_book'),
 ]
